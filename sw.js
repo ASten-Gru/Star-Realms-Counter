9f6e6d3c7a8ce338891526016d239ddf745feba3
ASten-Gru <andreas.stenzel@gmail.com>
2026-09-05 15:53:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="realm-counter-v18";
+const CACHE="realm-counter-v19";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg",
   "./background.webp",
   "./blob_art.webp",
